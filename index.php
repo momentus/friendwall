4af9e19ac030f03167a1aaf78729ab5e3d649fda
anna billstrom <banane@gmail.com>
2011-12-13 03:01:52 +0000

    adding tracking

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $(document).ready(function() {
 	$("#save-button").click(function() {
 		if(at != ''){
 			$.post('build.php', {'access_token':at});		
-			var t = setTimeout("publish()",1000);
+			publish();
 		} else {
 			auth();
 		}
@@ -51,7 +51,6 @@ $(document).ready(function() {
 
 function auth(){
 	top.location.href = '<?php if(isset($auth_uri)){ echo $auth_uri; }?>';
-
 }
 
 function publish(){
@@ -139,4 +138,16 @@ font-family: 'Convergence', sans-serif;
 					 xfbml: true});
 
 </script>
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27642799-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
